bd902c6047346def4a13cbbf88c6228272c3f697
abdulfetasuudi <suudiabdulfetah@gmail.com>
2024-03-29 01:21:37 +0300

    Change carousel type

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,68 +1,113 @@
 export default function Features() {
 	return (
-		<div className="py-32 flex flex-col gap-20 items-center bg-primary-content">
-			<div className=" w-[80%] flex flex-wrap gap-20 justify-between">
-				<p className="text-4xl font-black max-w-72">
-					Drive Into Adventure: Buy Your <span className="text-primary">Dream Car</span> Today!
-				</p>
-				<div>
-					<p className="max-w-96">
-						Bugatti epitomizes automotive excellence, merging
-						precision with passion in every creation. With an
-						illustrious heritage and relentless pursuit of
-						perfection, Bugatti crafts iconic luxury vehicles that
-						redefine automotive standards.
-					</p>
-					<button className="btn btn-primary mt-5 shadow-primary">
-						Learn More &rarr;
-					</button>
+		<div className="flex flex-col items-center justify-center pb-32">
+			<p className="text-4xl font-black max-w-64 mb-24">
+				Drive Into Adventure: Buy Your{" "}
+				<span className="text-primary">Dream Car</span> Today!
+			</p>
+			<div className="carousel w-[90%] max-w-screen-xl rounded-3xl">
+				<div id="item1" className="carousel-item w-full relative">
+					<img src="./bugatti1.jpg" className="w-full" />
+					<div className="absolute w-full h-full flex items-start justify-center pt-0 md:pt-10">
+						<div className="card md:scale-100 scale-50 max-w-96 max-h-72 text-primary-content glass">
+							<div className="card-body">
+								<h2 className="card-title">
+									Unparalleled Performance
+								</h2>
+								<p>
+									Exceptional performance, boasting powerful
+									engines, advanced aerodynamics
+								</p>
+								<div className="card-actions justify-end">
+									<button className="btn btn-primary">
+										Read Detail &rarr;
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div id="item2" className="carousel-item w-full relative">
+					<img src="./bugatti2.jpg" className="w-full" />
+					<div className="absolute w-full h-full flex items-start justify-center pt-0 md:pt-10">
+						<div className="card md:scale-100 scale-50 max-w-96 max-h-72 text-primary-content glass">
+							<div className="card-body">
+								<h2 className="card-title">
+									Exquisite Craftsmanship
+								</h2>
+								<p>
+									Meticulously handcrafted with the finest
+									materials and attention to detail. From the
+									luxurious interiors to the flawless exterior
+									finishes
+								</p>
+								<div className="card-actions justify-end">
+									<button className="btn btn-primary">
+										Read Detail &rarr;
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div id="item3" className="carousel-item w-full relative">
+					<img src="./bugatti3.jpg" className="w-full" />
+					<div className="absolute w-full h-full flex items-start justify-center pt-0 md:pt-10">
+						<div className="card md:scale-100 scale-50 max-w-96 max-h-72 text-primary-content glass">
+							<div className="card-body">
+								<h2 className="card-title">
+									Innovative Engineering
+								</h2>
+								<p>
+									Leveraging state-of-the-art technologies and
+									groundbreaking design concepts to optimize
+									performance, handling, and safety
+								</p>
+								<div className="card-actions justify-end">
+									<button className="btn btn-primary">
+										Read Detail &rarr;
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div id="item4" className="carousel-item w-full relative">
+					<img src="./bugatti4.jpg" className="w-full" />
+					<div className="absolute w-full h-full flex items-start justify-center pt-0 md:pt-10">
+						<div className="card md:scale-100 scale-50 max-w-96 max-h-72 text-primary-content glass">
+							<div className="card-body">
+								<h2 className="card-title">Iconic Design</h2>
+								<p>
+									Characterized by sleek lines, distinctive
+									curves, and aerodynamic shapes that not only
+									enhance performance but also embody the
+									brand&qous;s heritage and vision for automotive
+									excellence
+								</p>
+								<div className="card-actions justify-end">
+									<button className="btn btn-primary">
+										Read Detail &rarr;
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
-			<div className="carousel w-[80%] rounded-2xl">
-				<div id="slide1" className="carousel-item relative w-full">
-					<img src="./bugatti1.jpg" className="w-full" />
-					<div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-						<a href="#slide4" className="btn btn-circle">
-							❮
-						</a>
-						<a href="#slide2" className="btn btn-circle">
-							❯
-						</a>
-					</div>
-				</div>
-				<div id="slide2" className="carousel-item relative w-full">
-					<img src="./bugatti2.jpg" className="w-full" />
-					<div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-						<a href="#slide1" className="btn btn-circle">
-							❮
-						</a>
-						<a href="#slide3" className="btn btn-circle">
-							❯
-						</a>
-					</div>
-				</div>
-				<div id="slide3" className="carousel-item relative w-full">
-					<img src="./bugatti3.jpg" className="w-full" />
-					<div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-						<a href="#slide2" className="btn btn-circle">
-							❮
-						</a>
-						<a href="#slide4" className="btn btn-circle">
-							❯
-						</a>
-					</div>
-				</div>
-				<div id="slide4" className="carousel-item relative w-full">
-					<img src="./bugatti2.jpg" className="w-full" />
-					<div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-						<a href="#slide3" className="btn btn-circle">
-							❮
-						</a>
-						<a href="#slide1" className="btn btn-circle">
-							❯
-						</a>
-					</div>
-				</div>
+			<div className="flex justify-center w-full py-2 gap-2">
+				<a href="#item1" className="btn btn-xs">
+					1
+				</a>
+				<a href="#item2" className="btn btn-xs">
+					2
+				</a>
+				<a href="#item3" className="btn btn-xs">
+					3
+				</a>
+				<a href="#item4" className="btn btn-xs">
+					4
+				</a>
 			</div>
 		</div>
 	);
