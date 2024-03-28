244c6729c6d070d90665a5b6368115c3b3c2c264
abdulfetasuudi <suudiabdulfetah@gmail.com>
2024-03-28 11:23:44 +0300

    Add Hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,36 @@
+export default function Hero() {
+    return (
+		<>
+			<div
+				className="hero min-h-screen"
+				style={{
+					backgroundImage:
+						"url(https://cdn.luxe.digital/media/20240206073102/bugatti-price-list-ranking-review-luxe-digital-1200x600.jpg.webp)",
+				}}
+			>
+				<div className="hero-overlay bg-opacity-60"></div>
+				<div className="hero-content text-center text-neutral-content">
+					<div className="max-w-md">
+						<h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+						<p className="mb-5">
+							Provident cupiditate voluptatem et in. Quaerat
+							fugiat ut assumenda excepturi exercitationem quasi.
+							In deleniti eaque aut repudiandae et a id nisi.
+						</p>
+						<button className="btn btn-primary">Get Started</button>
+					</div>
+				</div>
+			</div>
+
+			<div className="card max-w-96 glass">
+				<div className="card-body">
+					<h2 className="card-title">Life hack</h2>
+					<p>How to park your car at your garage?</p>
+					<div className="card-actions justify-end">
+						<button className="btn btn-primary">Learn now!</button>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
