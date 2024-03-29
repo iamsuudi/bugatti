3cd9cc1c665137d88fc98f224b64ea25b4902ebb
abdulfetasuudi <suudiabdulfetah@gmail.com>
2024-03-29 16:56:34 +0300

    Use steps instead of cards

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,6 +1,6 @@
 export default function Detail() {
 	return (
-		<div className="py-32 flex flex-col gap-32 justify-center items-center">
+		<div className="py-32 px-10 flex flex-col gap-32 justify-center items-center">
 			<div className="w-fit flex flex-col items-center">
 				<h3 className="text-3xl font-black w-fit text-center">
 					How It <span className="text-primary">Works</span>
@@ -8,39 +8,47 @@ export default function Detail() {
 				<p className="w-fit">Buy With Following 3 Working Steps</p>
 			</div>
 			<div className="w-full flex flex-wrap justify-center items-center gap-24">
-				<div className="card max-w-80 hover:glass">
-					<div className="card-body">
-						<h2 className="card-title">Choose Location</h2>
-						<p>Choose Your Location And Nearest Store</p>
-						<div className="card-actions justify-end">
-							<button className="btn btn-primary">
-								See Now!
-							</button>
+				<ul className="steps steps-vertical">
+					<li className="step step-primary h-56 block">
+						<div className="collapse bg-base-200">
+							<input type="checkbox" />
+							<div className="collapse-title text-xl font-medium">
+								Choose Location
+							</div>
+							<div className="collapse-content">
+								<p>Choose Your Location And Nearest Store</p>
+							</div>
 						</div>
-					</div>
-				</div>
-				<div className="card max-w-80 hover:glass">
-					<div className="card-body">
-						<h2 className="card-title">Pick Up Date</h2>
-						<p>Select Your Pick Up Date And Time To Book Your Car</p>
-						<div className="card-actions justify-end">
-							<button className="btn btn-primary">
-								See Now!
-							</button>
+					</li>
+					<li className="step step-primary">
+						<div className="collapse bg-base-200">
+							<input type="checkbox" />
+							<div className="collapse-title text-xl font-medium">
+								Piack Up Date
+							</div>
+							<div className="collapse-content">
+								<p>
+									Select Your Pick Up Date And Time To Book
+									Your Car
+								</p>
+							</div>
 						</div>
-					</div>
-				</div>
-				<div className="card max-w-80 hover:glass">
-					<div className="card-body">
-						<h2 className="card-title">Book Your Car</h2>
-						<p>Book Your Car And We Will Deliver Directly To You</p>
-						<div className="card-actions justify-end">
-							<button className="btn btn-primary">
-								See Now!
-							</button>
+					</li>
+					<li className="step step-primary">
+						<div className="collapse bg-base-200">
+							<input type="checkbox" />
+							<div className="collapse-title text-xl font-medium">
+								Buy Your Car
+							</div>
+							<div className="collapse-content">
+								<p>
+									Book Your Car And We Will Deliver Directly
+									To You
+								</p>
+							</div>
 						</div>
-					</div>
-				</div>
+					</li>
+				</ul>
 			</div>
 		</div>
 	);
