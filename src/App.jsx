c26ecd550d0fde812feb4491450f8b8410505572
abdulfetasuudi <suudiabdulfetah@gmail.com>
2024-03-29 23:23:41 +0300

    Add darkMode toggling feature

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,23 @@ import Hero from "./components/Hero";
 import Detail from "./components/Detail";
 import Features from "./components/Features";
 import Footer from "./components/Footer";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [theme, setTheme] = useState(
+		localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+  );
+
+  useEffect(() => {
+		localStorage.setItem("theme", theme);
+		document
+			.querySelector("html")
+			.setAttribute("data-theme", localStorage.getItem("theme"));
+  }, [theme]);
+
 	return (
     <>
-      <Nav />
+      <Nav theme={ theme } setTheme={setTheme} />
       <Hero />
       <Detail />
       <Features />
