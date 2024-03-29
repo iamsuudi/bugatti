ee67e931408a94bd3e0a5af05dc5f06770ea2c86
abdulfetasuudi <suudiabdulfetah@gmail.com>
2024-03-29 09:11:14 +0300

    Deploy production

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Nav from "./components/Nav";
 import Hero from "./components/Hero";
 import Detail from "./components/Detail";
 import Features from "./components/Features";
+import Footer from "./components/Footer";
 
 function App() {
 	return (
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <Detail />
       <Features />
+      <Footer />
 		</>
 	);
 }
