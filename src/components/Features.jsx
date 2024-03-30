2d66236101c7cd1df3ad3b16a8bf4e519c8927f4
abdulfetasuudi <suudiabdulfetah@gmail.com>
2024-03-30 22:43:17 +0300

    Add text to features on home page

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -27,16 +27,81 @@ export default function Features() {
 				<div className=" w-[90%] max-w-screen-xl mx-auto">
 					<div className="carousel w-full rounded-box">
 						<div id="item1" className="carousel-item w-full">
-							<img src="./bugatti1.jpg" className="w-full" />
+							<div className="card w-full bg-base-300 shadow-xl">
+								<div className="card-body">
+									<h2 className="card-title">
+										Unrivaled Performance
+									</h2>
+									<p>
+										Powered by high-output engines that
+										deliver astonishing horsepower and
+										torque
+									</p>
+								</div>
+								<figure>
+									<img
+										src="./bugatti1.jpg"
+										className="w-full"
+									/>
+								</figure>
+							</div>
 						</div>
 						<div id="item2" className="carousel-item w-full">
-							<img src="./bugatti2.jpg" className="w-full" />
+							<div className="card w-full bg-base-300 shadow-xl">
+								<div className="card-body">
+									<h2 className="card-title">
+										Exquisite Design
+									</h2>
+									<p>
+										From the iconic horseshoe grille to the
+										sleek lines and curves
+									</p>
+								</div>
+								<figure>
+									<img
+										src="./bugatti2.jpg"
+										className="w-full"
+									/>
+								</figure>
+							</div>
 						</div>
 						<div id="item3" className="carousel-item w-full">
-							<img src="./bugatti3.jpg" className="w-full" />
+							<div className="card w-full bg-base-300 shadow-xl">
+								<div className="card-body">
+									<h2 className="card-title">
+										Luxurious Interiors
+									</h2>
+									<p>
+										Fine leather, carbon fiber, and exotic
+										woods adorn every surface
+									</p>
+								</div>
+								<figure>
+									<img
+										src="./bugatti3.jpg"
+										className="w-full"
+									/>
+								</figure>
+							</div>
 						</div>
 						<div id="item4" className="carousel-item w-full">
-							<img src="./bugatti4.jpg" className="w-full" />
+							<div className="card w-full bg-base-300 shadow-xl">
+								<div className="card-body">
+									<h2 className="card-title">
+										Advanced Technology
+									</h2>
+									<p>
+										Adaptive suspension systems to advanced
+										driver assistance features
+									</p>
+								</div>
+								<figure>
+									<img
+										src="./bugatti4.jpg"
+										className="w-full"
+									/>
+								</figure>
+							</div>
 						</div>
 					</div>
 					<div className="flex justify-center w-full py-2 gap-2">
