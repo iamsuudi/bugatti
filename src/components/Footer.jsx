e2b98d075fb11f14f971523aac46ddc4736d9e09
abdulfetasuudi <suudiabdulfetah@gmail.com>
2024-04-01 23:10:52 +0300

    Add image to contact page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -27,7 +27,7 @@ export default function Footer({location}) {
 				</nav>
 				<form>
 					<h6 className="footer-title">Newsletter</h6>
-					<fieldset className="form-control w-80">
+					<fieldset className="form-control max-w-80">
 						<label className="label">
 							<span className="label-text">
 								Enter your email address
--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,33 +1,53 @@
 import Section from "../components/Animate";
 
 export default function Contact() {
-    return (
-		<div className="py-52 px-20 flex flex-col gap-10 text-sm">
-			<Section>
-				<>
-					<h2 className="text-3xl font-black mb-2">Company Name</h2>
-					<p>BUGATTI AUTOMOBILES S.A.S.</p>
-				</>
-			</Section>
-			<Section>
-				<>
-					<h2 className="text-3xl font-black mb-2">Address</h2>
-					<p>1, Château Saint Jean </p>
-					<p>67120 Molsheim, Alsace France</p>
-				</>
-			</Section>
-			<Section>
-				<>
-					<h2 className="text-3xl font-black mb-2">Email Us</h2>
-					<p>info@bugatti.com</p>
-				</>
-			</Section>
-			<Section>
-				<>
-					<h2 className="text-3xl font-black mb-2">Call Us</h2>
-					<p>+33 388 04 56 00</p>
-				</>
-			</Section>
+	return (
+		<div className="hero py-52 w-[90%] max-w-screen-xl mx-auto">
+			<div className="hero-content flex-col gap-20 lg:flex-row-reverse">
+				<div className="text-center lg:text-left">
+					<img
+						src="./bugatti-contact.jpg"
+						className="w-full rounded-3xl shadow-2xl"
+					/>
+				</div>
+				<div className="card shrink-0 w-full max-w-sm">
+					<div className="flex flex-col gap-10 text-sm w-fit">
+						<Section>
+							<>
+								<h2 className="text-3xl font-black mb-2">
+									Company Name
+								</h2>
+								<p>BUGATTI AUTOMOBILES S.A.S.</p>
+							</>
+						</Section>
+						<Section>
+							<>
+								<h2 className="text-3xl font-black mb-2">
+									Address
+								</h2>
+								<p>1, Château Saint Jean </p>
+								<p>67120 Molsheim, Alsace France</p>
+							</>
+						</Section>
+						<Section>
+							<>
+								<h2 className="text-3xl font-black mb-2">
+									Email Us
+								</h2>
+								<p>info@bugatti.com</p>
+							</>
+						</Section>
+						<Section>
+							<>
+								<h2 className="text-3xl font-black mb-2">
+									Call Us
+								</h2>
+								<p>+33 388 04 56 00</p>
+							</>
+						</Section>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
