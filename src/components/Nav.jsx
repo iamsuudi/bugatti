078a24444e7ee40919ebeeaece01cbec4d5e50ae
suudi <suudiabdulfetah@gmail.com>
2024-04-06 16:48:48 +0300

    Update the nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,159 +3,135 @@ import MotionButton from "./MotionButton";
 import { Link } from "react-router-dom";
 
 export default function Nav({ theme, setTheme, location }) {
-	const toggleDarkMode = (e) => {
-		if (e.target.checked) {
-			console.log("now light so changing to dark");
-			setTheme("dark");
-			// localStorage.setItem("theme", "dark");
-			console.log(localStorage);
-		} else {
-			console.log("now dark so changing to light");
-			setTheme("light");
-			// localStorage.setItem("theme", "light");
-			console.log(localStorage);
-		}
-	};
+    const toggleDarkMode = (e) => {
+        if (e.target.checked) {
+            console.log("now light so changing to dark");
+            setTheme("dark");
+            // localStorage.setItem("theme", "dark");
+            console.log(localStorage);
+        } else {
+            console.log("now dark so changing to light");
+            setTheme("light");
+            // localStorage.setItem("theme", "light");
+            console.log(localStorage);
+        }
+    };
 
-	return (
-		<div className="navbar glass mx-[1.5vw] my-[1.5vw] py-3 px-[3vw] fixed w-[97vw] z-20 rounded-full">
-			<div className="navbar-start">
-				{location.pathname !== "/contact" && (
-					<MotionButton>
-						<button className="btn btn-primary hidden lg:block">
-							<Link to={"/contact"}>Contact US</Link>
-						</button>
-					</MotionButton>
-				)}
-				<div className="dropdown flex">
-					<div
-						tabIndex={0}
-						role="button"
-						className="btn btn-ghost lg:hidden"
-					>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							className="h-5 w-5"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke="currentColor"
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth="2"
-								d="M4 6h16M4 12h8m-8 6h16"
-							/>
-						</svg>
-					</div>
-					<ul
-						tabIndex={0}
-						className="menu menu-sm dropdown-content mt-16 z-[1] p-5 gap-2 shadow glass rounded-box w-52"
-					>
-						<li>
-							<Link to={"/"}>Homepage</Link>
-						</li>
-						<li>
-							<Link to={"/features"}>Features</Link>
-						</li>
-						<li>
-							<Link to={"/contact"}>Contact Us</Link>
-						</li>
-					</ul>
-					<div className="btn btn-ghost text-3xl flex gap-0">
-						<Link to={"/"}>
-							<span className="text-5xl">B</span>
-							<span>ugatti</span>
-						</Link>
-					</div>
-				</div>
-			</div>
-			<div className="navbar-center hidden lg:flex">
-				<ul className="menu menu-horizontal px-1">
-					<li>
-						<Link to={"/"}>Homepage</Link>
-					</li>
-					<li>
-						<Link to={"/features"}>Features</Link>
-					</li>
-					<li>
-						<Link to={"/contact"}>Contact Us</Link>
-					</li>
-				</ul>
-			</div>
-			<div className="navbar-end">
-				<button className="btn btn-ghost btn-circle">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						className="h-5 w-5"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke="currentColor"
-					>
-						<path
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							strokeWidth="2"
-							d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-						/>
-					</svg>
-				</button>
-				<button className="btn btn-ghost btn-circle">
-					<div className="indicator">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							className="h-5 w-5"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke="currentColor"
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth="2"
-								d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-							/>
-						</svg>
-						<span className="badge badge-xs badge-primary indicator-item"></span>
-					</div>
-				</button>
-				<button className="btn btn-ghost btn-circle">
-					<label className="swap swap-rotate">
-						{/* this hidden checkbox controls the state */}
-						<input
-							type="checkbox"
-							className="theme-controller"
-							value="synthwave"
-							checked={theme === "light" ? false : true}
-							onChange={toggleDarkMode}
-						/>
+    return (
+        <div className="navbar glass mx-[1.5vw] my-[1.5vw] py-3 px-[3vw] fixed w-[97vw] z-20 rounded-full">
+            <div className="navbar-start">
+                {location.pathname !== "/contact" && (
+                    <MotionButton>
+                        <button className="btn btn-primary hidden lg:block">
+                            <Link to={"/contact"}>Contact US</Link>
+                        </button>
+                    </MotionButton>
+                )}
+                <div className="dropdown flex">
+                    <div
+                        tabIndex={0}
+                        role="button"
+                        className="btn btn-ghost lg:hidden"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h8m-8 6h16"
+                            />
+                        </svg>
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content mt-16 z-[1] p-5 gap-2 shadow glass rounded-box w-52"
+                    >
+                        <li>
+                            <Link to={"/"}>Homepage</Link>
+                        </li>
+                        <li>
+                            <Link to={"/features"}>Features</Link>
+                        </li>
+                        <li>
+                            <Link to={"/contact"}>Contact Us</Link>
+                        </li>
+                    </ul>
+                    <div className="btn btn-ghost text-3xl flex gap-0">
+                        <Link to={"/"}>
+                            <span className="text-5xl">B</span>
+                            <span>ugatti</span>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <li>
+                        <Link to={"/"}>Homepage</Link>
+                    </li>
+                    <li>
+                        <Link to={"/features"}>Features</Link>
+                    </li>
+                    <li>
+                        <Link to={"/contact"}>Contact Us</Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <button className="btn btn-ghost btn-circle">
+                    <a href="https://github.com/iamsuudi/bugatti" target="_blank">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 496 512"
+                            className="w-6 h-6"
+                        >
+                            <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
+                        </svg>
+                    </a>
+                </button>
+                <button className="btn btn-ghost btn-circle">
+                    <label className="swap swap-rotate">
+                        {/* this hidden checkbox controls the state */}
+                        <input
+                            type="checkbox"
+                            className="theme-controller"
+                            value="synthwave"
+                            checked={theme === "light" ? false : true}
+                            onChange={toggleDarkMode}
+                        />
 
-						{/* sun icon */}
-						<svg
-							className="swap-off fill-current w-5 h-5"
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 24 24"
-						>
-							<path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
-						</svg>
+                        {/* sun icon */}
+                        <svg
+                            className="swap-off fill-current w-7 h-7"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                        >
+                            <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+                        </svg>
 
-						{/* moon icon */}
-						<svg
-							className="swap-on fill-current w-5 h-5"
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 24 24"
-						>
-							<path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
-						</svg>
-					</label>
-				</button>
-			</div>
-		</div>
-	);
+                        {/* moon icon */}
+                        <svg
+                            className="swap-on fill-current w-5 h-5"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                        >
+                            <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+                        </svg>
+                    </label>
+                </button>
+            </div>
+        </div>
+    );
 }
 
 Nav.propTypes = {
-	theme: PropTypes.string,
-	setTheme: PropTypes.func,
-	location: PropTypes.object,
+    theme: PropTypes.string,
+    setTheme: PropTypes.func,
+    location: PropTypes.object,
 };
